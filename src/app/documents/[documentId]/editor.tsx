f6325b7c9ae42794a00c7f78b003a587ca904193
Shravan Janwade <shravan_janwade@thbs.com>
2025-02-10 19:54:51 +0530

    TiptapExtensions Part1

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,8 @@
 'use client'
 import {useEditor,EditorContent} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 
 export const Editor=()=>{
     const editor=useEditor({
@@ -10,7 +12,11 @@ export const Editor=()=>{
                 class:"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-0 pr-14 pb-10 cursor-text"
             }
         },
-        extensions:[StarterKit],
+        extensions:[StarterKit,
+            TaskList,
+            TaskItem.configure({
+              nested: true,
+            }),],
         content:'<p>Hello World</p>'
     })
     return (
