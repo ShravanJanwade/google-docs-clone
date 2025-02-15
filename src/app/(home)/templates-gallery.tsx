c36dbec7c3fb17dc21429a10dcb85f803ed0a663
Shravan Janwade <shravanjanwade2252@gmail.com>
2025-02-15 16:01:29 +0530

    Create and Read documents

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -9,8 +9,25 @@ import {
 } from '@/components/ui/carousel'
 import { cn } from '@/lib/utils'
 import { templates } from '@/constants/templates'
+import { useRouter } from 'next/navigation'
+import { useMutation } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
+import { useState } from 'react';
 export const TemplatesGallery = () => {
-    const isCreating = false;
+    const router=useRouter();
+    const create=useMutation(api.documents.create);
+    const [isCreating,setIsCreating] = useState(false);
+
+    const onTemplateClick=(title:string,initialContent:string)=>{
+        setIsCreating(true);
+        create({title,initialContent}).then(
+            (documentId)=>{
+                router.push(`/documents/${documentId}`)
+            }
+        ).finally(()=>{
+            setIsCreating(false)
+        })
+    }
     return (
         <div className="bg-[#F1F3F4]">
             <div className="max-w-screen-xl mx-auto  px-16 py-6 flex flex-col gap-y-4">
@@ -29,6 +46,8 @@ export const TemplatesGallery = () => {
                                                 backgroundPosition: "center",
                                                 backgroundRepeat: "no-repeat"
                                             }}
+                                            disabled={isCreating}
+                                            onClick={()=>onTemplateClick(template.label,"")}
                                             className='size-full hover:border-blue-500 rounded-sm border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white'
                                         />
 
