af8b05bdb90443a4af3bfd5ac244b52b1088bc01
Shravan Janwade <shravanjanwade2252@gmail.com>
2025-02-16 18:19:01 +0530

    Notification Feature

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -9,6 +9,7 @@ import { BsFilePdf } from 'react-icons/bs';
 import { useEditorStore } from '@/store/use-editor-store';
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Avatars } from './avatars';
+import { Inbox } from './inbox';
 const FileMenu = () => {
     const { editor } = useEditorStore();
     const onDownload = (blob: Blob, filename: string) => {
@@ -145,6 +146,7 @@ export const Navbar = () => {
             </div>
             <div className='flex gap-3 items-center'>
                 <Avatars/>
+                <Inbox/>
             <OrganizationSwitcher
              afterLeaveOrganizationUrl='/' 
              afterCreateOrganizationUrl='/'
